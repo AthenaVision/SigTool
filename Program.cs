bbf9988ad8fcd7498638b43d3891ad5e5f3312fb
coltonon <cnpearson42@gmail.com>
2018-04-08 05:13:07 -0500

    bugs

--- Program.cs
+++ Program.cs
@@ -12,11 +12,15 @@ namespace SigTool
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            string filetoopen = "none";
+            if (args.Length > 0) filetoopen = args[0];
+            Application.Run(new Form1(filetoopen));
+          
         }
     }
 }
